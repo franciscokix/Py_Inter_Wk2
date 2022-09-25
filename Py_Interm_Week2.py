5f5082803fd741cff7c0da4f8a5f94eec8898f28
Frank Teixeira <teixeifr@sheridanc.on.ca>
2022-09-25 14:51:10 -0400

    Updated python script and README.md file

--- Py_Interm_Week2.py
+++ Py_Interm_Week2.py
@@ -1,11 +1,18 @@
 #Test __pychache__
 if __name__ == '__main__':
-    import os
+    #import libraries
+    import time
+    #define vars
     frank_list = []
+    #prompt for final number
+    try:
+        final_number = int(input('Enter final number: '))
+    except: 
+        print('Please enter an integer.  Exiting...')
+        time.sleep(3)
+        exit()
 
-    for i in range (0,5):
+    for i in range (0,final_number,2):
         frank_list.append(i)
 
-current_dir = os.curdir
-print(frank_list)
-print(current_dir)   
+print(f'list of even numbers from 0 to {final_number}: \n{frank_list}')
--- README.md
+++ README.md
@@ -2,3 +2,4 @@ F. Teixeira 18 Sep. 2022
 
 Readme.md:
 This is a repository & version control system test.
+Py_Interm_Week2.py is used to create a list of even numbers, from zero to a speficied value
